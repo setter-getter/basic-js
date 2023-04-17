3fb6222cf5c9612b21104faa3da1b00313a9bca1
setter-getter <dimkinyashchik@yandex.ru>
2023-04-17 11:16:58 +0300

    add new modules

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let encryptedMessage = '';
+    let j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let messageCharCode = message.charCodeAt(i);
+
+      if (messageCharCode >= 65 && messageCharCode <= 90) {
+        let keyCharCode = key.charCodeAt(j % key.length);
+
+        let encryptedCharCode = ((messageCharCode + keyCharCode - 130) % 26) + 65;
+        encryptedMessage += String.fromCharCode(encryptedCharCode);
+
+        j++;
+      } else {
+        encryptedMessage += message.charAt(i);
+      }
+    }
+
+    return this.isDirect ? encryptedMessage : encryptedMessage.split('').reverse().join('');
   }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let message = '';
+    let j = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      let encryptedCharCode = encryptedMessage.charCodeAt(i);
+
+      if (encryptedCharCode >= 65 && encryptedCharCode <= 90) {
+        let keyCharCode = key.charCodeAt(j % key.length);
+
+        let messageCharCode = ((encryptedCharCode - keyCharCode + 26) % 26) + 65;
+        message += String.fromCharCode(messageCharCode);
+
+        j++;
+      } else {
+        message += encryptedMessage.charAt(i);
+      }
+    }
+
+    return this.isDirect ? message : message.split('').reverse().join('');
+  }F
 }
 
 module.exports = {
